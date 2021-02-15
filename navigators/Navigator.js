ed7772891a0492bc56c4cfd4e931eb55278d9ee0
taneli1 <meikalainen31@gmail.com>
2021-02-15 15:35:49 +0200

    Project Init

---
+++ navigators/Navigator.js
@@ -0,0 +1,71 @@
+import React, {useContext} from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import {
+  getFocusedRouteNameFromRoute,
+  NavigationContainer,
+} from '@react-navigation/native';
+import {MainContext} from '../contexts/MainContext';
+import {Icon} from 'react-native-elements';
+import PlaceHolder from '../views/PlaceHolder';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const TabScreen = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          switch (route.name) {
+            case 'Home':
+              iconName = 'home';
+              break;
+            case 'Discover':
+              iconName = 'account-box';
+              break;
+            case 'Upload':
+              iconName = 'cloud-upload';
+              break;
+            case 'Profile':
+              iconName = 'account-box';
+              break;
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={PlaceHolder} />
+      <Tab.Screen name="Discover" component={PlaceHolder} />
+      <Tab.Screen name="Upload" component={PlaceHolder} />
+      <Tab.Screen name="Profile" component={PlaceHolder} />
+    </Tab.Navigator>
+  );
+};
+
+const StackScreen = () => {
+  return (
+    <>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={TabScreen}
+          options={({route}) => ({
+            headerTitle: getFocusedRouteNameFromRoute(route),
+          })}
+        />
+      </Stack.Navigator>
+    </>
+  );
+};
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <StackScreen />
+    </NavigationContainer>
+  );
+};
+
+export default Navigator;
---
+++ views/PlaceHolder.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function PlaceHolder() {
+  return (
+    <View>
+      <Text>Testing</Text>
+    </View>
+  )
+}
