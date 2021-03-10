8097d43eeec367cd0240a2fa2a7317be39f08066
taneli1 <meikalainen31@gmail.com>
2021-03-10 19:09:09 +0200

    Changes to UI

--- components/lists/List.js
+++ components/lists/List.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {FlatList, Text} from 'react-native';
+import {Dimensions, FlatList, StyleSheet, Text} from 'react-native';
 import PropTypes from 'prop-types';
 import PostDefault from '../listitems/PostDefault';
 import ProfilePost from '../listitems/ProfilePost';
@@ -74,12 +74,35 @@ const List = ({
   } else if (layout === 'discover') {
     return (
       <ScrollView>
-        <SearchBar placeholder="Type Here..." />
+        <SearchBar
+          lightTheme={true}
+          containerStyle={styles.search}
+          placeholder="Type Here..."
+        />
 
-        <Card>
-          <Card.Title>{tagTitle}</Card.Title>
+        <View style={styles.container}>
+          <View style={styles.textContainer}>
+            <TouchableOpacity
+              style={{alignSelf: 'flex-start'}}
+              onPress={() => {
+                navigation.navigate('Discover More', {data: mediaArray});
+              }}
+            >
+              <Text style={styles.tagHeader}>{tagTitle}</Text>
+            </TouchableOpacity>
+            <Text
+              onPress={() => {
+                navigation.navigate('Discover More', {data: mediaArray});
+              }}
+              style={styles.button}
+            >
+              See more &gt;
+            </Text>
+          </View>
 
           <FlatList
+            showsHorizontalScrollIndicator={false}
+            ListFooterComponent={<View style={{marginLeft: 30}} />}
             horizontal={true}
             data={mediaArray}
             keyExtractor={(item, index) => index.toString()}
@@ -87,18 +110,31 @@ const List = ({
               <DiscoverDefault navigation={navigation} data={item} />
             )}
           />
-          <Button
-            title="See more"
-            onPress={() => {
-              navigation.navigate('Discover More', {data: mediaArray});
-            }}
-          >
-            See more
-          </Button>
-        </Card>
-        <Card>
-          <Card.Title>{tagTitle2}</Card.Title>
+        </View>
+
+        <View style={styles.container}>
+          <View style={styles.textContainer}>
+            <TouchableOpacity
+              style={{alignSelf: 'flex-start'}}
+              onPress={() => {
+                navigation.navigate('Discover More', {data: mediaArray2});
+              }}
+            >
+              <Text style={styles.tagHeader}>{tagTitle2}</Text>
+            </TouchableOpacity>
+            <Text
+              onPress={() => {
+                navigation.navigate('Discover More', {data: mediaArray2});
+              }}
+              style={styles.button}
+            >
+              See more &gt;
+            </Text>
+          </View>
+
           <FlatList
+            showsHorizontalScrollIndicator={false}
+            ListFooterComponent={<View style={{marginLeft: 30}} />}
             horizontal={true}
             data={mediaArray2}
             keyExtractor={(item, index) => index.toString()}
@@ -106,18 +142,32 @@ const List = ({
               <DiscoverDefault navigation={navigation} data={item} />
             )}
           />
-          <Button
-            title="See more"
-            onPress={() => {
-              navigation.navigate('Discover More', {data: mediaArray2});
-            }}
-          >
-            See more
-          </Button>
-        </Card>
-        <Card>
-          <Card.Title>{tagTitle3}</Card.Title>
+        </View>
+
+        <View style={styles.container}>
+          <View style={styles.textContainer}>
+            <TouchableOpacity
+              style={{alignSelf: 'flex-start'}}
+              onPress={() => {
+                navigation.navigate('Discover More', {data: mediaArray3});
+              }}
+            >
+              <Text style={styles.tagHeader}>{tagTitle3}</Text>
+            </TouchableOpacity>
+            <Text
+              onPress={() => {
+                navigation.navigate('Discover More', {data: mediaArray3});
+              }}
+              style={styles.button}
+            >
+              See more &gt;
+            </Text>
+          </View>
+
           <FlatList
+            showsHorizontalScrollIndicator={false}
+            ListFooterComponent={<View style={{marginLeft: 30}} />}
+            style={{paddingBottom: 50}}
             horizontal={true}
             data={mediaArray3}
             keyExtractor={(item, index) => index.toString()}
@@ -125,15 +175,7 @@ const List = ({
               <DiscoverDefault navigation={navigation} data={item} />
             )}
           />
-          <Button
-            title="See more"
-            onPress={() => {
-              navigation.navigate('Discover More', {data: mediaArray3});
-            }}
-          >
-            See more
-          </Button>
-        </Card>
+        </View>
       </ScrollView>
     );
   } else if (layout === 'discoverMore') {
@@ -149,6 +191,40 @@ const List = ({
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'flex-start',
+    marginTop: 50,
+  },
+  textContainer: {
+    width: Dimensions.get('window').width,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  tagHeader: {
+    backgroundColor: Colors.primary,
+    color: Colors.white,
+    borderColor: Colors.white,
+    borderWidth: 1,
+    padding: 5,
+    paddingLeft: 10,
+    borderRadius: 5,
+    fontSize: 16,
+    marginLeft: 15,
+  },
+  button: {
+    color: Colors.primary,
+    fontSize: 16,
+    marginRight: 25,
+  },
+  search: {
+    width: Dimensions.get('window').width - 60,
+    marginLeft: 30,
+    marginTop: 50,
+  },
+});
 
 List.propTypes = {
   navigation: PropTypes.object,
